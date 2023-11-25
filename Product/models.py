7e25adf06dbef263b9620da3ee35686f9a34c12a
Hrithik Hadawale <hrithikhadawale75@gmail.com>
2023-11-26 02:35:35 +0530

    fefe

--- Product/models.py
+++ Product/models.py
@@ -25,15 +25,15 @@ veg_NonVeg_choice= (
 class Product(models.Model):
     name = models.CharField(max_length=25)
     img = models.ImageField(upload_to='Products/')
-    category = models.ManyToManyField(Category)
-    subcategory = models.ForeignKey(SubCategory,on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category,null=True,blank=True)
+    subcategory = models.ForeignKey(SubCategory,on_delete=models.CASCADE,null=True,blank=True)
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2,max_digits=9999)
+    price = models.DecimalField(decimal_places=2,max_digits=9999,null=True,blank=True)
     
     veg_nonveg = models.CharField(max_length=20, choices=veg_NonVeg_choice,null=True,blank=True)
 
-    meta_tags = models.TextField()
-    meta_description = models.TextField()
+    meta_tags = models.TextField(null=True,blank=True)
+    meta_description = models.TextField(null=True,blank=True)
     
     def __str__(self):
         return self.name
